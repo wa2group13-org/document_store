0c70395a7fd95720b9e3910d9fab38e1ecfaefcb
Brendon Mendicino <brendonmendicino@yahoo.it>
2024-06-07 00:08:14 +0200

    feat: added topic configuration

--- document_store/src/main/kotlin/it/polito/wa2/g13/document_store/DocumentStoreApplication.kt
+++ document_store/src/main/kotlin/it/polito/wa2/g13/document_store/DocumentStoreApplication.kt
@@ -1,27 +1,15 @@
 package it.polito.wa2.g13.document_store
 
+import it.polito.wa2.g13.document_store.properties.KafkaConfigProperties
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class DocumentStoreApplication {
-//    @Bean
-//    fun commandLineRunner(repo: DocumentRepository): CommandLineRunner {
-//        return CommandLineRunner {
-//            repo.save(
-//                DocumentMetadata(
-//                    0,
-//                    "asdfklj",
-//                    10,
-//                    "SIUM",
-//                    Calendar.getInstance().time,
-//                    DocumentFile(0, ByteArray(10))
-//                )
-//            )
-//            repo.findAll().toList().forEach { println(it) }
-//        }
-//    }
-}
+@EnableConfigurationProperties(
+    KafkaConfigProperties::class,
+)
+class DocumentStoreApplication
 
 fun main(args: Array<String>) {
     runApplication<DocumentStoreApplication>(*args)
