2f8f095d27b520622ee891f791da6e16bab9ec65
Brendon Mendicino <brendonmendicino@yahoo.it>
2024-06-11 16:41:59 +0200

    feat: added springdoc dependency

--- document_store/src/main/kotlin/it/polito/wa2/g13/document_store/config/SecurityConfig.kt
+++ document_store/src/main/kotlin/it/polito/wa2/g13/document_store/config/SecurityConfig.kt
@@ -3,6 +3,8 @@ package it.polito.wa2.g13.document_store.config
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Profile
+import org.springframework.core.env.Environment
+import org.springframework.core.env.Profiles
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.http.SessionCreationPolicy
@@ -11,11 +13,18 @@ import org.springframework.security.web.SecurityFilterChain
 @Configuration
 @EnableWebSecurity
 @Profile("!no-security")
-class SecurityConfig {
+class SecurityConfig(
+    private val environment: Environment,
+) {
     @Bean
     fun filterChain(http: HttpSecurity): SecurityFilterChain {
         return http
             .authorizeHttpRequests {
+                // If api-docs is enabled allow resource access to get the api
+                if (environment.acceptsProfiles(Profiles.of("api-docs"))) {
+                    it.requestMatchers("/v3/api-docs/**", "/swagger-ui.html", "/swagger-ui/**").permitAll()
+                }
+
                 it.anyRequest().authenticated()
             }
             .oauth2ResourceServer { it.jwt {} }
