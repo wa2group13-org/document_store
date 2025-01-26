8d2d93fb209748a34359628a4bff23128a19078c
Brendon Mendicino <brendonmendicino@yahoo.it>
2025-01-26 17:59:45 +0100

    version: 1.2.1 -> 1.3.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,7 +18,7 @@ plugins {
 val springCloudVersion by extra("2023.0.2")
 
 group = "it.polito.wa2.g13"
-version = "1.2.1"
+version = "1.3.0"
 
 java {
     sourceCompatibility = JavaVersion.VERSION_21
